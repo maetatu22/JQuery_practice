f4f86f7528429b164e60da4f82225fece054daa4
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-15 22:50:31 +0900

    children findメソッド練習

---
+++ practice10/script.js
@@ -0,0 +1,9 @@
+$(() => {
+  $("#find-method").click(function() {
+    $("#wrapper").find("a").css("color", "red");
+  })
+
+  $("#children-method").click(function() {
+    $("#wrapper").children("a").fadeOut();
+  })
+})
