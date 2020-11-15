2f97491b00b8eebcd969a5c490ca7ca56649d779
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-15 22:36:39 +0900

    変数練習

---
+++ practice9/script.js
@@ -0,0 +1,11 @@
+$(() => {
+  $(".btn").click(function() {
+    let $title = $("#title");
+
+    $title.css("color", "red");
+    $title.html('こんばんは、にんじゃわんこさん');
+    $title.fadeOut(1000);
+
+    $("#text").css("color", "blue").html('<h3>jQueryをマスターしましょう！</h3>').fadeOut(1000);
+  })
+})
