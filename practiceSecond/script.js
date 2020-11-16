a330f8e11f8506b98e0bb602614e110eac45326d
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-16 17:46:34 +0900

    アコーディオン作成

--- practiceSecond/script.js
+++ practiceSecond/script.js
@@ -26,8 +26,14 @@ $(() => {
 
     if($answer.hasClass("open")){
       $answer.removeClass("open");
+      $answer.slideUp();
+      $(this).find("span").text("+");
     }else{
       $answer.addClass("open");
+      $answer.slideDown();
+      $(this).find("span").text("-");
     }
   });
+
+
 });
