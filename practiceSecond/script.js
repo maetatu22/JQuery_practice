c8df7457528b5b4236e533e3a73270798b92a1ca
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-16 14:53:30 +0900

    モーダルを閉じる

--- practiceSecond/script.js
+++ practiceSecond/script.js
@@ -6,4 +6,9 @@ $(() => {
   $(".signup-show").click(function() {
     $("#signup-modal").fadeIn();
   });
+
+  $(".close-modal").click(function() {
+    $("#login-modal").fadeOut();
+    $("#signup-modal").fadeOut();
+  })
 });
