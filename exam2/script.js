bb20ef8dccccc1ccae2dee36860c2db7e2db9332
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-16 21:49:32 +0900

    Q & A表示＆非表示練習　準備

---
+++ exam2/script.js
@@ -0,0 +1,24 @@
+$(function() {
+
+
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+
+  $('#close-modal').click(function() {
+    $('#signup-modal').fadeOut();
+  });
+
+
+  $('.lesson').hover(
+    function() {
+      $(this).find('.text-contents').addClass('text-active');
+    },
+    function() {
+      $(this).find('.text-contents').removeClass('text-active');
+    }
+  );
+  
+  
+
+});
