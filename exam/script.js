e5c9ca9ceca9f1728b1461fa4d65b90dd1a5e354
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-16 21:46:01 +0900

    説明文表示&非表示

--- exam/script.js
+++ exam/script.js
@@ -6,4 +6,13 @@ $(() => {
   $("#close-modal").click(() => {
     $("#signup-modal").fadeOut(1000);
   })
+
+  $(".lesson").hover(
+    function() { 
+      $(this).find(".text-contents").addClass("text-active");
+    },
+    function() {
+      $(this).find(".text-contents").removeClass("text-active");
+    });
+
 });
