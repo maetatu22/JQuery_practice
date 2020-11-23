2d3d8afb4f24c73c93ba57d479fcccba628a9a4c
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-23 15:37:16 +0900

    スライドクリック　チェンジボタン条件分岐

---  advanced5/script.js
+++  advanced5/script.js
@@ -4,6 +4,13 @@ $(() => {
     var clickedIndex = $('.index-btn').index($(this));
     $('.slide').eq(clickedIndex).addClass('active');
 
+    var slideIndex = $(".slide").index($('.active'));
+    $(".change-btn").show();
+    if(slideIndex == 0){
+      $(".prev-btn").hide();
+    }else if(slideIndex == 3){
+      $(".next-btn").hide();
+    }
 
   });
 
