abbedb4b70b8d7c5cac65cabc053436c76e785ef
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-25 19:22:27 +0900

    アニメーション　font-size変更

---
+++  animation1/script.js
@@ -0,0 +1,15 @@
+$(() => {
+  $(".social-icon").hover(
+    function(){
+      $(this).animate({
+        "font-size": "30px"
+      }, 300);
+    },
+    function(){
+      $(this).animate({
+        "font-size": "24px"
+      }, 300);
+    }
+    
+  )
+})
